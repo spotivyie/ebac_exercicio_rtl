89ab5a54603a45c76cc3a050ec65cc115158a72d
Eduarda Cardoso Brandão <eduardac.brandao@hotmail.com>
2023-12-11 20:15:43 -0400

    commit

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    it('Deve adicionar o componente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentário', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
